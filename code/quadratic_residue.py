6bb8c59e909fefae402ce7ee38a8d76e027f0c7d
gkmjack <gkmjack@outlook.com>
2019-06-02 01:43:18 -0400

    Recursive Legendre symbol computation using quadratic reciprocity

--- code/quadratic_residue.py
+++ code/quadratic_residue.py
@@ -1,12 +1,31 @@
 from modular import mod;
-from primality import miller_rabin_test;
+from primality import is_prime, factorize, miller_rabin_test;
 
-def legendre_symbol(a, p):
-    """Compute the Legendre symbol (a/p) where p is an odd prime."""
+def legendre_symbol(a, p, recursive = False):
+    """Compute the Legendre symbol (a/p) where p is an odd prime.
+    When 'recursive' option is set, use the law of quadratic reciprocity;
+    otherwise, use Euler's criterion."""
     if (p == 1) or (p%2 == 0) or (miller_rabin_test(p) == 0):
         raise Exception("Invalid Legendre symbol");
     # Check that p is indeed an odd prime
-    result = mod(p, a, (p-1)>>1);
-    if result > 1:
-        return -1;
+    a %= p;
+    if recursive:
+        if (a == 0 or a == 1):
+            return a;
+        if a == 2:
+            return legendre_symbol(a, p, False);
+        if is_prime(a):
+            if (a%4==3 and p%4==3):
+                return -legendre_symbol(p, a, True);
+            else:
+                return legendre_symbol(p, a, True);
+        factors = factorize(a);
+        result = 1;
+        for i in factors:
+            if factors[i]%2:
+                result *= legendre_symbol(i, p, True);
+    else:
+        result = mod(p, a, (p-1)>>1);
+        if result == (p-1):
+            result = -1;
     return result;
